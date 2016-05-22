ad78fef80499d037b8c7e17c0eb214e970fac2af
Juan Pablo Salamanca Ramírez <jpsalamarcara@unal.edu.co>
2016-05-22 03:47:55 -0500

    Init semantic! :D

---
+++ src/semantic/EvalVisitor.java
@@ -0,0 +1,9 @@
+package semantic;
+
+import grammar.LITEBaseVisitor;
+
+/**
+ * Created by juanpablo on 22/05/16.
+ */
+public class EvalVisitor extends LITEBaseVisitor<Value> {
+}
---
+++ src/semantic/Value.java
@@ -0,0 +1,9 @@
+package semantic;
+
+/**
+ * Created by juanpablo on 22/05/16.
+ */
+public class Value {
+
+
+}
